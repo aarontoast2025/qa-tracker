a5526c6c2835b755fdaae064cf17cd35b214691b
aarontoast2025 <aarontoast2025@users.noreply.github.com>
2026-01-27 03:30:51 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      },
+      {
+        // Allow CORS for the bookmarklet script in public
+        source: "/qa-form.js",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
